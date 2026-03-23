62e564a3ec846dde033d4048788393a45e2f26b1
order <39032044+orderrrr@users.noreply.github.com>
2026-03-23 20:26:59 +0200

    bump to v0.2.5

--- Formula/ztach.rb
+++ Formula/ztach.rb
@@ -1,16 +1,16 @@
 class Ztach < Formula
   desc "Terminal session multiplexer with viewport support"
   homepage "https://github.com/orderrrr/ztach"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/orderrrr/ztach/releases/download/v0.2.4/ztach-0.2.4-macos-arm64.tar.gz"
-      sha256 "ede40bbe87096f3f4b95c1120e867d80ba27d8ba29f08cb06a23801447374444"
+      url "https://github.com/orderrrr/ztach/releases/download/v0.2.5/ztach-0.2.5-macos-arm64.tar.gz"
+      sha256 "b173b1b9b5362e232deab2e5d7f4f6a314d17a0f8be5b2e917627824ca4b72ce"
     else
-      url "https://github.com/orderrrr/ztach/releases/download/v0.2.4/ztach-0.2.4-macos-x86_64.tar.gz"
-      sha256 "c6b917f986ebf9bc846621d87219bf97cf9873299b4b5af55b3f72033205d327"
+      url "https://github.com/orderrrr/ztach/releases/download/v0.2.5/ztach-0.2.5-macos-x86_64.tar.gz"
+      sha256 "a84a5a286ef43f48a72f50d87a86f20f1e878fe4efc7df3548fc6d973618b3e5"
     end
   end
 
